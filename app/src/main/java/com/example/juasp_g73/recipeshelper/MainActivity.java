ac465f66c8594e6a926a33efc544644271ddc081
julienasp <julien.aspirot@usherbrooke.ca>
2016-12-06 15:34:03 -0500

    working

--- app/src/main/java/com/example/juasp_g73/recipeshelper/MainActivity.java
+++ app/src/main/java/com/example/juasp_g73/recipeshelper/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.juasp_g73.recipeshelper;
 
 import android.app.AlertDialog;
 import android.app.PendingIntent;
-import android.content.DialogInterface;
-import android.content.IntentFilter;
-import android.content.Intent;
+import android.content.*;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.BitmapDrawable;
+import android.nfc.*;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -15,13 +16,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.nfc.FormatException;
-import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
-import android.nfc.NfcAdapter;
-import android.nfc.Tag;
 import android.nfc.tech.Ndef;
-import android.content.Context;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -32,59 +27,109 @@ public class MainActivity extends AppCompatActivity {
     private NfcAdapter nfcAdapter = null;
     private Tag mytag;
     private Context ctx;
+    ImageView iv_nfc;
     private PendingIntent mPendingIntent = null;
     private IntentFilter writeTagFilters[];
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            if (action.equals(NfcAdapter.ACTION_ADAPTER_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(NfcAdapter.EXTRA_ADAPTER_STATE, NfcAdapter.STATE_OFF);
+                switch (state) {
+                    case NfcAdapter.STATE_OFF:
+                        Toast.makeText(ctx,"NFC not available!", Toast.LENGTH_LONG).show();
+                        iv_nfc.setImageResource(R.drawable.nfcred300x300);
+                        break;
+                    case NfcAdapter.STATE_TURNING_OFF:
+                        break;
+                    case NfcAdapter.STATE_ON:
+                        Toast.makeText(ctx,"NFC available!", Toast.LENGTH_LONG).show();
+                        iv_nfc.setImageResource(R.drawable.nfcgreen300x300);
+                        break;
+                    case NfcAdapter.STATE_TURNING_ON:
+                        break;
+                }
+            }
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        ImageView iv_nfc = (ImageView) findViewById(R.id.imageView_nfc);
+        iv_nfc = (ImageView) findViewById(R.id.imageView_nfc);
         setSupportActionBar(toolbar);
         ctx = this;
 
+        try {
+            nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+            if(nfcAdapter != null && nfcAdapter.isEnabled()){
+                Toast.makeText(this,"NFC available!", Toast.LENGTH_LONG).show();
 
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
-        if(nfcAdapter != null && nfcAdapter.isEnabled()){
-            Toast.makeText(this,"NFC available!", Toast.LENGTH_LONG).show();
-            //iv_nfc.setImageResource(R.drawable.nfcgreen);
-        }
-        else{
-            Toast.makeText(this,"NFC not available!", Toast.LENGTH_LONG).show();
-            //iv_nfc.setBackground(ctx.getDrawable(R.drawable.nfcred));
+                //Prevent out of memory errors
+                ((BitmapDrawable)iv_nfc.getDrawable()).getBitmap().recycle();
 
-            AlertDialog.Builder alertbox = new AlertDialog.Builder(ctx);
-            alertbox.setTitle("NFC disabled");
-            alertbox.setMessage("You must turn on your phone's NFC functionnality to use this app.");
-            alertbox.setPositiveButton("Turn On", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                        Intent intent = new Intent(Settings.ACTION_NFC_SETTINGS);
-                        startActivity(intent);
-                    } else {
-                        Intent intent = new Intent(Settings.ACTION_WIRELESS_SETTINGS);
-                        startActivity(intent);
+                iv_nfc.setImageResource(R.drawable.nfcgreen300x300);
+            }
+            else{
+                Toast.makeText(this,"NFC not available!", Toast.LENGTH_LONG).show();
+
+                //Prevent out of memory errors
+                ((BitmapDrawable)iv_nfc.getDrawable()).getBitmap().recycle();
+
+                iv_nfc.setImageResource(R.drawable.nfcred300x300);
+
+                //AlertDialog to turn on NFC
+                AlertDialog.Builder alertbox = new AlertDialog.Builder(ctx);
+                alertbox.setTitle("NFC disabled");
+                alertbox.setMessage("You must turn on your phone's NFC functionnality to use this app.");
+                alertbox.setPositiveButton("Turn On", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                            Intent intent = new Intent(Settings.ACTION_NFC_SETTINGS);
+                            startActivity(intent);
+                        } else {
+                            Intent intent = new Intent(Settings.ACTION_WIRELESS_SETTINGS);
+                            startActivity(intent);
+                        }
                     }
-                }
-            });
-            alertbox.setNegativeButton("Back", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
+                });
+                alertbox.setNegativeButton("Back", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
-                }
-            });
-            alertbox.show();
+                    }
+                });
+                alertbox.show();
+            }
+            mPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,
+                    getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+            IntentFilter if_nfc_state_changed = new IntentFilter(NfcAdapter.ACTION_ADAPTER_STATE_CHANGED);
+            this.registerReceiver(mReceiver, if_nfc_state_changed);
+
+            IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+
+            tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
+            writeTagFilters = new IntentFilter[] { tagDetected };
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        mPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,
-                getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+    }
 
-        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
-        tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
-        writeTagFilters = new IntentFilter[] { tagDetected };
-
+    @Override
+    protected void onNewIntent(Intent intent){
+        if(NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())){
+            mytag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            Toast.makeText(this, "new intent: " + mytag.toString(), Toast.LENGTH_LONG ).show();
+        }
     }
 
     @Override
