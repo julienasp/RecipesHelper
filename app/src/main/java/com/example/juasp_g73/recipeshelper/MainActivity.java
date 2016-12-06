6a7f2d3757c6966018af5a316cbf153e392c232a
julienasp <julien.aspirot@usherbrooke.ca>
2016-12-06 11:48:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/juasp_g73/recipeshelper/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.juasp_g73.recipeshelper;
+
+import android.app.PendingIntent;
+import android.content.IntentFilter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.nfc.FormatException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.content.Context;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String NFC_TOKEN_MIME_TYPE = "application/vnd.wfa.wsc";
+    private NfcAdapter nfcAdapter = null;
+    private Tag mytag;
+    private Context ctx;
+    private PendingIntent mPendingIntent = null;
+    private IntentFilter writeTagFilters[];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if(nfcAdapter != null && nfcAdapter.isEnabled()){
+            Toast.makeText(this,"NFC available!", Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(this,"NFC not available!", Toast.LENGTH_LONG).show();
+        }
+
+        mPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,
+                getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
+        writeTagFilters = new IntentFilter[] { tagDetected };
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
