ddb30fdc51f7001cc00f8cd3bce4938a5f4b74a5
julienasp <julien.aspirot@usherbrooke.ca>
2017-01-02 18:12:59 -0500

    working on it

--- app/src/main/java/com/example/juasp_g73/recipeshelper/DirectionActivity.java
+++ app/src/main/java/com/example/juasp_g73/recipeshelper/DirectionActivity.java
@@ -1,17 +1,13 @@
 package com.example.juasp_g73.recipeshelper;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.*;
 import com.squareup.picasso.Picasso;
 import core.Direction;
 import core.Ingredient;
@@ -19,17 +15,22 @@ import core.Recipe;
 import core.Tool;
 import org.javatuples.Pair;
 
+import java.util.Locale;
 import java.util.Vector;
 
 public class DirectionActivity extends AppCompatActivity {
-    Recipe r;
-    int stepIndex = 0;
-    Direction currentDirection;
-    TextView direction_step;
-    TextView direction_description;
-    ImageView direction_image_url;
-    GridView direction_ingredients;
-    GridView direction_tools;
+    private Recipe r;
+    private int stepIndex = 0;
+    private Direction currentDirection;
+    private TextView direction_step;
+    private TextView direction_description;
+    private ImageView direction_image_url;
+    private ImageView iv_item_image_url;
+    private GridView direction_ingredients;
+    private GridView direction_tools;
+    private TextView tv_timer;
+    private ImageView iv_timer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +44,45 @@ public class DirectionActivity extends AppCompatActivity {
         direction_ingredients = (GridView) findViewById(R.id.direction_ingredients);
         direction_tools = (GridView) findViewById(R.id.direction_tools);
         direction_description.setMovementMethod(new ScrollingMovementMethod());
+        iv_item_image_url = (ImageView) findViewById(R.id.iv_item_image_url);
+        tv_timer = (TextView) findViewById(R.id.tv_timer);
+        iv_timer = (ImageView) findViewById(R.id.iv_timer);
+
+        //direction_tools OnItemClickListener
+        direction_tools.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Tool t = (Tool) direction_tools.getItemAtPosition(position);
+                if(t.getImage_url() != null) {
+                    Picasso.with(getApplicationContext())
+                            .load(t.getImage_url())
+                            .into(iv_item_image_url);
+                }else{
+                    Picasso.with(getApplicationContext())
+                            .load(R.drawable.phototbd)
+                            .resize(50, 50)
+                            .centerCrop()
+                            .into(iv_item_image_url);
+                }
+                t.showHint();
+                iv_timer.setVisibility(View.VISIBLE);
+                tv_timer.setVisibility(View.VISIBLE);
+                new CountDownTimer(15000, 1000) {
+
+                    public void onTick(long millisUntilFinished) {
+                        tv_timer.setText("seconds remaining: " + millisUntilFinished / 1000);
+                    }
+
+                    public void onFinish() {
+                        tv_timer.setText("");
+                        tv_timer.setVisibility(View.INVISIBLE);
+                        iv_timer.setVisibility(View.INVISIBLE);
+                        iv_item_image_url.setVisibility(View.INVISIBLE);
+                    }
+                }.start();
+
+            }
+        });
 
 
         Bundle extras = getIntent().getExtras();
@@ -50,42 +90,51 @@ public class DirectionActivity extends AppCompatActivity {
              r = (Recipe) extras.getSerializable("recipe");
 
             Log.i("DirectionActivity","[Recipe#id:"+r.getId()+"] has " + r.getDirections().size() + " directions");
-
-
-            //Hydrating the interface
-            currentDirection = r.getDirections().get(stepIndex);
-            if(currentDirection.getImage_url() != null) {
-                Picasso.with(this)
-                        .load(currentDirection.getImage_url())
-                        .into(direction_image_url);
-            }
-
-            //attributes are not null?
-            if (  currentDirection.getOrder() != null && currentDirection.getDescription() != null )
-            {
-                direction_step.setText("STEP #" + currentDirection.getOrder().toString());
-                direction_description.setText(currentDirection.getDescription());
-            }
-
-            //ingredient vector empty?
-            if(generateIngredientsFromDirection(currentDirection).size() > 0){
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                        android.R.layout.test_list_item, generateIngredientsFromDirection(currentDirection));
-
-                direction_ingredients.setAdapter(adapter);
-            }else direction_ingredients.setVisibility(View.INVISIBLE);
-
-            //tool vector empty?
-            if(currentDirection.getDirection_tools().size() > 0){
-                ArrayAdapter<Tool> adapter = new ArrayAdapter<Tool>(this,
-                        android.R.layout.test_list_item, currentDirection.getDirection_tools());
-
-                direction_tools.setAdapter(adapter);
-            }else direction_tools.setVisibility(View.INVISIBLE);
-
+            hydrateView();
         }
 
     }
+
+    private void hydrateView() {
+        if(r != null){
+            //Hydrating the interface
+            try {
+                currentDirection = r.getDirections().get(stepIndex);
+
+                if(currentDirection.getImage_url() != null) {
+                    Picasso.with(this)
+                            .load(currentDirection.getImage_url())
+                            .into(direction_image_url);
+                }
+
+                //attributes are not null?
+                if (  currentDirection.getOrder() != null && currentDirection.getDescription() != null )
+                {
+                    direction_step.setText(String.format("step #%d", currentDirection.getOrder()).toUpperCase(Locale.getDefault()));
+                    direction_description.setText(currentDirection.getDescription());
+                }
+
+                //ingredient vector empty?
+                if(generateIngredientsFromDirection(currentDirection).size() > 0){
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                            android.R.layout.test_list_item, generateIngredientsFromDirection(currentDirection));
+
+                    direction_ingredients.setAdapter(adapter);
+                }else direction_ingredients.setVisibility(View.INVISIBLE);
+
+                //tool vector empty?
+                if(currentDirection.getDirection_tools().size() > 0){
+                    ArrayAdapter<Tool> adapter = new ArrayAdapter<>(this,
+                            android.R.layout.test_list_item, currentDirection.getDirection_tools());
+
+                    direction_tools.setAdapter(adapter);
+                }else direction_tools.setVisibility(View.INVISIBLE);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     private Vector<String> generateIngredientsFromDirection(Direction d){
         Vector<String> ingredients = new Vector<>();
 
