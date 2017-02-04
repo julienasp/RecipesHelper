be10301a4345721e4b39e777038058959485a5a9
Julien Aspirot <julien.aspirot@usherbrooke.ca>
2017-02-04 11:39:43 -0500

    added javadoc

--- app/src/main/java/dao/RecipeDaoTasker.java
+++ app/src/main/java/dao/RecipeDaoTasker.java
@@ -12,8 +12,14 @@ import core.Recipe;
 import java.text.DecimalFormat;
 
 /**
- * Created by JUASP-G73 on 18/12/2016.
- */
+* RecipeDaoTasker is a child of AsyncTask<Void, Void, Void>, this class help us find a direction in the database.
+* This class is used to make sure that when we are hydrating a Recipe from the database it doesn't block the UI
+* This is an AsyncTask for the RecipeDao.
+*
+* @author  Julien Aspirot
+* @version 1.0
+* @since   2017-02-04 
+*/
 public class RecipeDaoTasker extends AsyncTask<Void, Void, Void> {
     private ProgressDialog progress;
     private Integer id;
@@ -22,6 +28,10 @@ public class RecipeDaoTasker extends AsyncTask<Void, Void, Void> {
     private Context c;
     private long startTime;
 
+    /**
+   * The constructor recipe a ProgressDialog, RecipeId and the android context  
+   * 
+   */
     public RecipeDaoTasker(ProgressDialog progress, Integer recipeId, Activity a, Context c) {
         this.progress = progress;
         this.id = recipeId;
@@ -35,12 +45,20 @@ public class RecipeDaoTasker extends AsyncTask<Void, Void, Void> {
         return null;
     }
 
+    /**
+   * This method is executed before the doInBackground work, we show the progressDialog to the user
+   * 
+   */
     public void onPreExecute() {
         //Chrono start
         this.startTime = System.nanoTime();
         progress.show();
     }
 
+     /**
+   * This method is executed after the doInBackground work, we dismiss the progressDialog and we redirect the user to another Activity
+   * 
+   */
     public void onPostExecute(Void unused) {
         progress.dismiss();
         Intent i = new Intent(c, RecipeActivity.class);
