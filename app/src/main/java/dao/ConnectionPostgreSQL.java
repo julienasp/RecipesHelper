24ed552cb011fc795d27ffc4fe3e22193690d233
Julien Aspirot <julien.aspirot@usherbrooke.ca>
2017-02-04 11:30:34 -0500

    javadoc added

--- app/src/main/java/dao/ConnectionPostgreSQL.java
+++ app/src/main/java/dao/ConnectionPostgreSQL.java
@@ -3,14 +3,23 @@ package dao;
 import android.util.Log;
 import java.sql.*;
 /**
- * Created by JUASP-G73 on 06/12/2016.
- */
+* ConnectionPostgreSQL knows all the detail about the database connection
+*
+* @author  Julien Aspirot
+* @version 1.0
+* @since   2017-02-04 
+*/
 public class ConnectionPostgreSQL {
     private static String url = "jdbc:postgresql://10.0.2.2:5432/IFT744";
     private static String user = "postgres";
     private static String passwd = "PG123!";
     private static Connection connect;
 
+    
+   /**
+   * This method is used to get the instance of the database connection  
+   * @return Connection it returns a connection object
+   */
     public static Connection getInstance(){
 
         if(connect == null){
