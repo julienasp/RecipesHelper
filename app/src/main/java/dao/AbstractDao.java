893d90766f7f3f78f9ba2a24f4ba44b5e686779c
Julien Aspirot <julien.aspirot@usherbrooke.ca>
2017-02-04 11:28:11 -0500

    Javadoc added

--- app/src/main/java/dao/AbstractDao.java
+++ app/src/main/java/dao/AbstractDao.java
@@ -3,8 +3,12 @@ package dao;
 import java.sql.Connection;
 
 /**
- * Created by JUASP-G73 on 06/12/2016.
- */
+* AbstractDao is an abstract class that shares the PostgreSQL connection
+*
+* @author  Julien Aspirot
+* @version 1.0
+* @since   2017-02-04 
+*/
 public abstract class AbstractDao<T> {
     protected Connection connect = ConnectionPostgreSQL.getInstance();
     public abstract T find(Integer id);
